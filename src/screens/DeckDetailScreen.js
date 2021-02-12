35db3838f546e7399570e044a39d33880982f022
Albert Vila Calvo <vilacalvo.albert@gmail.com>
2021-02-12 18:07:25 +0100

    DeckDetailScreen - add Delete button that navigates back to home

--- src/screens/DeckDetailScreen.js
+++ src/screens/DeckDetailScreen.js
@@ -1,14 +1,25 @@
-import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import React, {useEffect} from 'react';
+import {SafeAreaView, Text, TouchableOpacity} from 'react-native';
 import CustomStatusBar from '../components/CustomStatusBar';
 
-const DeckDetailScreen = ({route}) => {
+const DeckDetailScreen = ({route, navigation}) => {
+  const deleteDeck = () => {
+    navigation.goBack();
+  };
+
+  useEffect(() => {
+    navigation.setOptions({title: 'TODO'});
+  }, [navigation]);
+
   return (
     <>
       <CustomStatusBar />
       <SafeAreaView>
         <Text>Deck</Text>
         <Text>ID: {route.params.deckId}</Text>
+        <TouchableOpacity onPress={deleteDeck}>
+          <Text>Delete Deck</Text>
+        </TouchableOpacity>
       </SafeAreaView>
     </>
   );
