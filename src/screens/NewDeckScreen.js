eae4ac5c9d24276f185fcf9099454ad4f1381145
Albert Vila Calvo <vilacalvo.albert@gmail.com>
2021-02-15 12:20:54 +0100

    NewDeckScreen - add form to create new deck

--- src/screens/NewDeckScreen.js
+++ src/screens/NewDeckScreen.js
@@ -1,19 +1,39 @@
-import React from 'react'
-import { SafeAreaView, Text, TouchableOpacity } from 'react-native'
+import React, { useState } from 'react'
+import { SafeAreaView, TextInput, StyleSheet, Button } from 'react-native'
 import CustomStatusBar from '../components/CustomStatusBar'
+import { useNavigation } from '@react-navigation/native'
 
 const NewDeckScreen = () => {
+  const navigation = useNavigation()
+  const [input, setInput] = useState('')
+
+  const onPress = () => {
+    navigation.goBack()
+  }
+
   return (
     <>
       <CustomStatusBar />
-      <SafeAreaView>
-        <Text>Create New Deck</Text>
-        <TouchableOpacity>
-          <Text>Create Deck</Text>
-        </TouchableOpacity>
+      <SafeAreaView style={styles.container}>
+        <TextInput
+          style={styles.textInput}
+          placeholder="New Deck Name"
+          onChangeText={(text) => setInput(text)}
+          defaultValue={input}
+        />
+        <Button
+          title="Create New Deck"
+          disabled={input.trim() === ''}
+          onPress={onPress}
+        />
       </SafeAreaView>
     </>
   )
 }
 
 export default NewDeckScreen
+
+const styles = StyleSheet.create({
+  container: { paddingHorizontal: 40, paddingVertical: 40 },
+  textInput: { height: 40 },
+})
