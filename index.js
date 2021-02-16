7b77754ac726a2f668914000d28b773732d7359a
Albert Vila Calvo <vilacalvo.albert@gmail.com>
2021-02-16 13:25:24 +0100

    MobX configure useProxies ifavailable

--- index.js
+++ index.js
@@ -6,5 +6,11 @@ import 'react-native-gesture-handler' // https://reactnavigation.org/docs/gettin
 import { AppRegistry } from 'react-native'
 import App from './App'
 import { name as appName } from './app.json'
+import { configure } from 'mobx'
+
+// MobX config - https://mobx.js.org/configuration.html
+configure({
+  useProxies: 'ifavailable',
+})
 
 AppRegistry.registerComponent(appName, () => App)
