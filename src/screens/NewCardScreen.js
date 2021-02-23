d3182b4c431c2655516bf64a31ac30a0a136e6ba
Albert Vila Calvo <vilacalvo.albert@gmail.com>
2021-02-23 18:51:22 +0100

    NewCardScreen TextInput keyboard next focus and submit

--- src/screens/NewCardScreen.js
+++ src/screens/NewCardScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import { TextInput, StyleSheet } from 'react-native'
 import sharedStyles from '../styles/sharedStyles'
 import useDeckStore from '../deck/useDeckStore'
@@ -7,6 +7,8 @@ import CustomSafeAreaView from '../components/CustomSafeAreaView'
 import CustomButton from '../components/styled/CustomButton'
 
 const NewCardScreen = ({ route, navigation }) => {
+  const answerInput = useRef()
+
   const [question, setQuestion] = useState('')
   const [answer, setAnswer] = useState('')
 
@@ -15,6 +17,9 @@ const NewCardScreen = ({ route, navigation }) => {
   const deck = deckStore.getDeck(deckId)
 
   const onPress = () => {
+    if (question.trim() === '' || answer.trim() === '') {
+      return
+    }
     deck.addCard(new Card(question, answer))
     navigation.goBack()
   }
@@ -27,12 +32,17 @@ const NewCardScreen = ({ route, navigation }) => {
         placeholder="Question"
         onChangeText={(text) => setQuestion(text)}
         defaultValue={question}
+        returnKeyType="next"
+        onSubmitEditing={() => answerInput.current.focus()}
       />
       <TextInput
+        ref={answerInput}
         style={[sharedStyles.textInput, styles.textInputAnswer]}
         placeholder="Answer"
         onChangeText={(text) => setAnswer(text)}
         defaultValue={answer}
+        returnKeyType="done"
+        onSubmitEditing={onPress}
       />
       <CustomButton
         text="Add Card"
